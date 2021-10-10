ba5dda55b2bb0db2ba6418cc07415cd6831cb33d
mrnicos96 <76992643+mrnicos96@users.noreply.github.com>
2021-10-10 20:16:47 +0300

    RequestWindows

---
+++ GEO Json to MySQL Converter/Utils/RequestWindows.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace GEO_Json_to_MySQL_Converter.Utils
+{
+    public class RequestWindows
+    {
+        public static bool RequestFile(out string fileName)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog { RestoreDirectory = true };
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+                fileName = dialog.FileName;
+            else
+                fileName = String.Empty;
+            return result.Value;
+        }
+
+        public static void RequestQuestion(string text, out bool isNewDB)
+        {
+            isNewDB = MessageBox.Show(text, "GEO Json to MySQL Converter", MessageBoxButton.YesNo,
+                MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.Yes;
+        }
+
+        public static bool RequestNewDB(string title, string type, out string fileName)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog { Title = title, Filter = type };
+            var result = dialog.ShowDialog();
+            if (result.HasValue && result.Value)
+                fileName = dialog.FileName;
+            else
+                fileName = String.Empty;
+            return result.Value;
+        }
+    }
+}
---
+++ GEO Json to MySQL Converter/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using GEO_Json_to_MySQL_Converter.Utils;
+
+namespace GEO_Json_to_MySQL_Converter.ViewModels
+{
+    public class MainViewModel : BusyViewModel
+    {
+        private RelayCommand openFileCommand;
+
+        public MainViewModel()
+        { 
+
+        }
+
+        public RelayCommand OpenFileCommand => openFileCommand ??
+                  (openFileCommand = new RelayCommand((o) =>
+                  {
+                      try
+                      {
+                          OnBusy("Waiting ...");
+                          RequestWindows.RequestFile(out string file);
+                          RequestWindows.RequestQuestion("Создать новую базу данных ?", out bool isNewDB);
+                          RequestWindows.RequestQuestion("Начать считывание файла с первой строки?", out bool isNewAttempt);
+                          if (isNewDB==true)
+                          {
+                              RequestWindows.RequestNewDB("Выбор БД", "db files (*.db)|*.db", out string pathDB);
+                              СontinueDesirialaseFile(file, pathDB, isNewAttempt);
+                          }
+                          else
+                          {
+                              RequestWindows.RequestFile(out string pathDB);
+                              DesirialaseFile(file, pathDB, isNewAttempt);
+                          }
+                      }
+                      catch (Exception ex)
+                      {
+                          MessageBox.Show($"{ex.Message} " +
+                               "Пришлите мне письмо с описание выших действи вызваших эту ошибку. " +
+                              @"bosup@bk.ru" + " Спасибо!",
+                              "GEO Json to MySQL Converter - Ошибка!",
+                              MessageBoxButton.OK, MessageBoxImage.Warning);
+                      }
+                      finally
+                      {
+
+                          OffBusy();
+                      }
+                  }));
+    }
+}
